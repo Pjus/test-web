a8036abbcc762b0751dc519e2ed60e0b5a3a4e20
Pjus <wnstjd117@naver.com>
2021-10-30 01:56:37 +0900

    add kor font

--- cert/views.py
+++ cert/views.py
@@ -12,6 +12,10 @@ from edu.models import Product
 from payment.models import PurchasedItem
 # Create your views here.
 
+
+from reportlab.pdfgen import canvas
+from datetime import datetime
+
 class CertListView(ListView):
     model = PurchasedItem
     paginate_by = 10
@@ -27,12 +31,13 @@ class GeneratePDF(View):
         print(current_user.name)
         print(current_user.email)
 
-
+        category_name = product_name.category
+        category_name.encode('utf-8')
         context = {
             "product_name": product_name.name,
             "customer_name": current_user.name,
             "cert": True,
-            "category": product_name.category,
+            "category": category_name,
             "today": datetime.today().strftime('%Y/%m/%d'),
         }
         
@@ -45,5 +50,32 @@ class GeneratePDF(View):
             if download:
                 content = "attachment; filename='%s'" %(filename)
             response['Content-Disposition'] = content
+            response.encoding = 'utf-8'
             return response
         return HttpResponse("Not found")
+
+def pdf_dw(request, pk):                                  
+    
+    # Create the HttpResponse object 
+    response = HttpResponse(content_type='application/pdf') 
+
+    # This line force a download
+    response['Content-Disposition'] = 'attachment; filename="1.pdf"' 
+
+    # READ Optional GET param
+    get_param = request.GET.get('name', 'World')
+
+    # Generate unique timestamp
+    ts = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    p = canvas.Canvas(response)
+
+    # Write content on the PDF 
+    p.drawString(100, 500, "한글 " + get_param + " (Dynamic PDF) - " + ts ) 
+
+    # Close the PDF object. 
+    p.showPage() 
+    p.save() 
+
+    # Show the result to the user    
+    return response
